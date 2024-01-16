69f09852863c340186405b3ed289288eb52e8880
David Smith <cpmpdavidsmith@icloud.com>
2024-01-16 14:39:13 -0500

    prac

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int? a = null;
+            int? b = 7;
+            Double? c = null;
+            Double? d = 4;
+            bool? val = new bool?();
+            DateTime? Start = DateTime.Today;
+            DateTime? End = null;
+            Console.WriteLine("showing values of nullables: {0}, {1}, {2}, {3}" ,a,b,c,d);
+            Console.WriteLine("A Nullable Boolean Variable: {0}", val);
+            Console.WriteLine(Start);
+            Console.WriteLine("We don't know yet:", End);
+            Console.Read();
+        }
+    }
+}
